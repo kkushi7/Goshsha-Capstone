bcdee17e51a947e0c133164d1f0a3930ef96002d
DingxinTao0417 <144394567+DingxinTao0417@users.noreply.github.com>
2024-10-28 23:53:13 -0700

    Add files via upload

---
+++ Goshsha Capstone/ScrapBookViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ScrapBookViewController.swift
+//  Goshsha Capstone
+//
+//  Created by Athena Yap on 10/23/24.
+//
+
+import Foundation
+import UIKit
+
+class ScrapBookViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+
+    }
+}
